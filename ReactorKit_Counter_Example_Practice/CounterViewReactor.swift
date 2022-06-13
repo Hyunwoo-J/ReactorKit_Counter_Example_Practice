f447bf85b10979138319d613cb24920890c370cf
Hyunwoo <heoun3089@gmail.com>
2022-06-13 10:37:12 +0900

    feat: CounterViewReactor의 Action 정의

---
+++ ReactorKit_Counter_Example_Practice/CounterViewReactor.swift
@@ -0,0 +1,31 @@
+//
+//  CounterViewReactor.swift
+//  ReactorKit_Counter_Example_Practice
+//
+//  Created by Hyunwoo Jang on 2022/06/13.
+//
+
+import Foundation
+
+import ReactorKit
+import RxSwift
+
+
+final class CounterViewReactor: Reactor {
+  
+  /// 사용자가 + 버튼을 눌렀을 때 어떤 형식으로 Reactor에 전달할지 정의한다.
+  enum Action {
+    case increase // +버튼을 눌렀을 때는 Reactor에 increase 액션을 전달
+    case decrease // -버튼을 눌렀을 때는 Reactor에 decrease 액션을 전달
+  }
+  
+  enum Mutation {
+    
+  }
+  
+  struct State {
+    
+  }
+  
+  let initialState: State = State()
+}
